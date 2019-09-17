d4b8d651a5334ad576f19fdd2b545e318ed1c0ce
José Carlos <39632709+kinhosz@users.noreply.github.com>
2019-09-17 12:53:07 -0300

    Disjoint Set

--- Data structures/disjointSet.cpp
+++ Data structures/disjointSet.cpp
@@ -5,26 +5,26 @@ typedef vector<int> vi;
 class DSU{
 
 private: vi p,rank;
-public: 
-  void create(int N){
-    rank.assign(N,0);
-    for(int i=0;i<N;i++) p.push_back(i);
-  }
-  int unionFind(int i){
-    if(p[i] == i) return i;
-    else{
-      rank[i] = 0;
-      return (p[i] = unionFind(p[i]));
-    }
-  }
-  bool isSameSet(int i,int j){
-    return (unionFind(p[i]) == unionFind(p[j]));
-  }
-  void unionSet(int u,int v){
-    if(rank[u] > rank[v]) swap(u,v);
-    if(!isSameSet(u,v)){
-      p[u] = unionFind(p[v]);
-      rank[u]++;
-    }
-  }
+public:
+	void create(int N){
+		rank.assign(N,1);
+		p.resize(N);
+		for(int i=0;i<N;i++) p[i] = i;
+	}
+	int find(int i){
+		return (p[i] == i?i:(p[i] = find(p[i])));
+	}
+	bool isSameSet(int u,int v){
+
+		return (find(p[u]) == find(p[v]));
+	}
+	void unionSet(int u,int v){
+		u = find(p[u]);
+		v = find(p[v]);
+		if(!isSameSet(u,v)){
+			if(rank[u] > rank[v]) swap(u,v);
+			p[u] = v;
+			rank[v]+= rank[u];
+		}
+	}
 };
