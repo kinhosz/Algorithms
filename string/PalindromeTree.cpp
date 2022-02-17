b972ef55e78360d18c5121ae8183853dc494858e
José Carlos <39632709+kinhosz@users.noreply.github.com>
2022-02-17 00:18:00 -0300

    upd EERTREE

--- string/PalindromeTree.cpp
+++ string/PalindromeTree.cpp
@@ -1,20 +1,41 @@
 class pTree{
-public:
-
-	int s[MAXN], Link[MAXN], Len[MAXN], Edge[MAXN][SIGMA];
-	int node, cursor, n;
-	ll cnt[MAXN];
+private:
+	int MAXN;
+	int SIGMA;
 
 	void init(){
-		s[n++]=-1;
+		s.resize(MAXN);
+		Link.resize(MAXN);
+		Len.resize(MAXN);
+		Edge.resize(MAXN, vector<int>(SIGMA, 0));
+		cnt.resize(MAXN, 0);
+		n = 0;
+		cursor = 0;
+
+		s[n++] = -2; // empty string
+		s[n++]=-1; // imaginary string
 		Link[0]=1; Len[0]=0;
 		Link[1]=1; Len[1]=-1;
 		node=2;
 	}
 
 	int getLink(int v){
-		while(s[n-Len[v]-2]!=s[n-1]) v=Link[v];
+		while(v != 1 && s[n-Len[v]-2]!=s[n-1]) v=Link[v];
 		return v;
+	}
+
+public:
+
+	vector<int> s, Link, Len;
+	vector<vector<int>> Edge;
+	int node, cursor, n;
+	vector<ll> cnt;
+
+	pTree(int maxn, int sigma){
+		MAXN = maxn + 3;
+		SIGMA = sigma;
+
+		this->init();
 	}
 
 	void addLetter(int c){
@@ -34,12 +55,17 @@ public:
 		cursor=Edge[cursor][c];
 	}
 
-	void clear(){
-		for(int i=0;i<=node;i++){
-			cnt[i]=0;
-			memset(Edge[i], 0, sizeof Edge[i]);
+	ll propagate(){
+		ll ans = 0;
+
+		for(int i=node-1;i>=2;i--){
+			ll x = cnt[i];
+			int link = Link[i];
+			if(link > 1) cnt[link] += x;
+
+			ans += x;
 		}
-		n=0;
-		cursor=0;
+
+		return ans;
 	}
 };
