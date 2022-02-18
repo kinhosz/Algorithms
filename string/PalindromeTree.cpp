a3b2f65c953256295d55bdb947a32405cb33f184
José Carlos <39632709+kinhosz@users.noreply.github.com>
2022-02-17 21:02:40 -0300

    upd eertree

--- string/PalindromeTree.cpp
+++ string/PalindromeTree.cpp
@@ -1,71 +1,37 @@
-class pTree{
-private:
-	int MAXN;
-	int SIGMA;
+struct eertree {
+	vector<vector<int>> t;
+	int n, last, sz;
+	vector<int> s, len, link;
+	vector<ll> qt;
 
-	void init(){
-		s.resize(MAXN);
-		Link.resize(MAXN);
-		Len.resize(MAXN);
-		Edge.resize(MAXN, vector<int>(SIGMA, 0));
-		cnt.resize(MAXN, 0);
-		n = 0;
-		cursor = 0;
-
-		s[n++] = -2; // empty string
-		s[n++]=-1; // imaginary string
-		Link[0]=1; Len[0]=0;
-		Link[1]=1; Len[1]=-1;
-		node=2;
+	eertree(int N) {
+		t.resize(N+2, vector<int>(26));
+		s = len = link = vector<int>(N+2);
+		qt = vector<ll>(N+2);
+		s[0] = -1;
+		link[0] = 1, len[0] = 0, link[1] = 1, len[1] = -1;
+		sz = 2, last = 0, n = 1;
 	}
 
-	int getLink(int v){
-		while(v != 1 && s[n-Len[v]-2]!=s[n-1]) v=Link[v];
-		return v;
-	}
-
-public:
-
-	vector<int> s, Link, Len;
-	vector<vector<int>> Edge;
-	int node, cursor, n;
-	vector<ll> cnt;
-
-	pTree(int maxn, int sigma){
-		MAXN = maxn + 3;
-		SIGMA = sigma;
-
-		this->init();
-	}
-
-	void addLetter(int c){
-		s[n++]=c;
-		cursor=getLink(cursor);
-
-		if(!Edge[cursor][c]){
-			Len[node]=Len[cursor]+2;
-			Link[node]=Edge[getLink(Link[cursor])][c];
-			cnt[node]++;
-			Edge[cursor][c]=node++;
+	void add(char c) {
+		s[n++] = c -= 'a';
+		while (s[n-len[last]-2] != c) last = link[last];
+		if (!t[last][c]) {
+			int prev = link[last];
+			while (s[n-len[prev]-2] != c) prev = link[prev];
+			link[sz] = t[prev][c];
+			len[sz] = len[last]+2;
+			t[last][c] = sz++;
 		}
-		else{
-			cnt[Edge[cursor][c]]++;
-		}
-
-		cursor=Edge[cursor][c];
+		qt[last = t[last][c]]++;
 	}
-
-	ll propagate(){
-		ll ans = 0;
-
-		for(int i=node-1;i>=2;i--){
-			ll x = cnt[i];
-			int link = Link[i];
-			if(link > 1) cnt[link] += x;
-
-			ans += x;
+	int size() { return sz-2; }
+	ll propagate() {
+		ll ret = 0;
+		for (int i = n; i > 1; i--) {
+			qt[link[i]] += qt[i];
+			ret += qt[i];
 		}
-
-		return ans;
+		return ret;
 	}
 };
